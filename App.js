a91e3ae171f056f5ec8723780e213338c5645eca
Florence Shelley <smc.florence@gmail.com>
2021-10-09 00:35:20 -0400

    Add personal solution to styling exercise

--- App.js
+++ App.js
@@ -1,112 +1,41 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
+const App = () => (
+  <SafeAreaView>
+    <View style={styles.header}>
+      <Text style={styles.headerText}>
+        Here are some boxes of different colours
       </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+      <View style={[styles.box, styles.cyan]}>
+        <Text style={styles.boxText}>Cyan #2aa198</Text>
+      </View>
+      <View style={[styles.box, styles.blue]}>
+        <Text style={styles.boxText}>Blue #268bd2</Text>
+      </View>
+      <View style={[styles.box, styles.magenta]}>
+        <Text style={styles.boxText}>Magenta #d33682</Text>
+      </View>
+      <View style={[styles.box, styles.orange]}>
+        <Text style={styles.boxText}>Orange #cb4b16</Text>
+      </View>
     </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+  </SafeAreaView>
+);
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  header: {marginVertical: 5, paddingVertical: 10, paddingHorizontal: 20},
+  headerText: {fontWeight: 'bold'},
+  box: {
+    marginVertical: 5,
+    paddingVertical: 10,
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  boxText: {fontWeight: 'bold', color: '#fff'},
+  cyan: {backgroundColor: '#2aa198'},
+  blue: {backgroundColor: '#268bd2'},
+  magenta: {backgroundColor: '#d33682'},
+  orange: {backgroundColor: '#cb4b16'},
 });
 
 export default App;
